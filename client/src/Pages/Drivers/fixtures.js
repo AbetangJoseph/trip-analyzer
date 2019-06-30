6456cf391e8c8d59959dd87b64e005dca4a20791
Abetang Joseph <josephabetang@gmail.com>
2019-06-30 23:05:32 +0100

    cut out p tags for the driver info into fixture.js to form an object to loop through

---
+++ client/src/Pages/Drivers/fixtures.js
@@ -0,0 +1,26 @@
+export const driverDetails = [
+  {
+    key: "Gender",
+    value: "gender"
+  },
+  {
+    key: "Email",
+    value: "email"
+  },
+  {
+    key: "Phone",
+    value: "phone"
+  },
+  {
+    key: "Address",
+    value: "address"
+  },
+  {
+    key: "DOB",
+    value: "DOB"
+  },
+  {
+    key: "Agent",
+    value: "agent"
+  }
+];
--- client/src/Pages/Drivers/index.jsx
+++ client/src/Pages/Drivers/index.jsx
@@ -1,9 +1,7 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import Listgroup from "../../Components/Listgroup";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faSteering} from '@fortawesome/free-solid-svg-icons';
 import Card from "../../Components/Card";
+import { driverDetails } from "./fixtures";
 
 export default function Drivers() {
   const [drivers, setDrivers] = useState([]);
@@ -13,7 +11,6 @@ export default function Drivers() {
     const getDrivers = async () => {
       const fetchDrivers = await fetch("/api/drivers");
       const driversResponse = await fetchDrivers.json();
-
       let drivers = driversResponse;
       setDrivers(drivers.data);
       setDriver(drivers.data[0]);
@@ -25,8 +22,6 @@ export default function Drivers() {
     setDriver(driver);
   };
 
-  const { address, agent, DOB, email, gender, name, phone } = driver;
-
   return (
     <div style={{ margin: "15px" }}>
       <div
@@ -36,48 +31,34 @@ export default function Drivers() {
           padding: "30px"
         }}
       >
-        <h3 style={{ color: "#2ECC71", marginBottom: "70px" }}>Drivers</h3>
-        <h5
-          className="text-muted"
-          style={{ color: "#2ECC71", marginTop: "30px" }}
-        >
-          DecaDriver Details
-          {/* <FontAwesomeIcon
-            icon={faSteering}
-            style={{ color: 'gray', fontSize: '25px' }}
-          /> */}
-        </h5>
+        <h3 style={{ color: "#2ECC71" }}>Drivers</h3>
       </div>
 
       <div className="row">
-        <div class="col-4" style={{ padding: "0px" }}>
-          <Listgroup drivers={drivers} click={changeContent} />
+        <div className="col-4" style={{ padding: "0px" }}>
+          <Listgroup
+            drivers={drivers}
+            click={changeContent}
+            active={driver.driverID}
+          />
         </div>
 
         <div className="col-8">
           <Card
             cardWidth="col-sm-12"
-            cardTitle={name}
+            cardTitle={driver["name"]}
             cardBody={
               <div>
-                <p>
-                  <b>Gender:</b> {gender}
-                </p>
-                <p>
-                  <b>Email:</b> {email}
-                </p>
-                <p>
-                  <b>Phone:</b> {phone}
-                </p>
-                <p>
-                  <b>DOB:</b> {DOB}
-                </p>
-                <p>
-                  <b>Agent:</b> {agent}
-                </p>
-                <p>
-                  <b>Address:</b> {address}
-                </p>
+                {driverDetails.map((driverInfo, index) => (
+                  <p key={index}>
+                    <span className="text-muted" style={{ fontSize: "12px" }}>
+                      {`${driverInfo.key}:`}
+                    </span>{" "}
+                    <span style={{ fontSize: "14px" }}>
+                      {driver[driverInfo.value]}
+                    </span>
+                  </p>
+                ))}
               </div>
             }
           />
