818379dbf5047c7a68c17070deaf530a5c292e8f
Abetang Joseph <josephabetang@gmail.com>
2019-06-30 22:35:10 +0100

    import getVehicle function and replace getDriver on get vehicle and restart typescript with yarn tsc

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,55 +1,61 @@
-import { Router } from 'express';
-import { getTrips, getDrivers, getDriver, getTrip } from '../controllers/index';
+import { Router } from "express";
+import {
+  getTrips,
+  getDrivers,
+  getDriver,
+  getTrip,
+  getVehicle
+} from "../controllers/index";
 
 const router = Router();
 
-router.get('/', function(_req, res, _next) {
-  res.status(200).json({ message: 'All is well' });
+router.get("/", function(_req, res, _next) {
+  res.status(200).json({ message: "All is well" });
 });
 
-router.get('/trips', (_req, res) => {
+router.get("/trips", (_req, res) => {
   const trips = getTrips();
 
   res.status(200).json({ data: trips });
 });
 
-router.get('/trip/:tripID', (req, res) => {
+router.get("/trip/:tripID", (req, res) => {
   try {
     const data = getTrip(req.params.tripID);
 
     res.status(200).json({ data });
   } catch {
-    res.status(404).json({ error: 'Trip not found' });
+    res.status(404).json({ error: "Trip not found" });
   }
 });
 
-router.get('/drivers', (_req, res) => {
+router.get("/drivers", (_req, res) => {
   const drivers = getDrivers();
 
   res.status(200).json({ data: drivers });
 });
 
-router.get('/driver/:driverID', (req, res) => {
+router.get("/driver/:driverID", (req, res) => {
   try {
     const driver = getDriver(req.params.driverID);
 
     res.status(200).json({ data: driver });
   } catch {
-    res.status(404).json({ error: 'Driver not found' });
+    res.status(404).json({ error: "Driver not found" });
   }
 });
 
-router.get('/vehicle/:vehicleID', (req, res) => {
+router.get("/vehicle/:vehicleID", (req, res) => {
   try {
-    const vehicle = getDriver(req.params.vehicleID);
+    const vehicle = getVehicle(req.params.vehicleID);
 
     res.status(200).json({ data: vehicle });
   } catch {
-    res.status(404).json({ error: 'Vehicle not found' });
+    res.status(404).json({ error: "Vehicle not found" });
   }
 });
 
-router.get('/stats', (_req, res) => {
+router.get("/stats", (_req, res) => {
   res.status(200).json({
     data: {
       male: 30,
@@ -58,8 +64,8 @@ router.get('/stats', (_req, res) => {
       noOfNonCashTrips: 24,
       billedTotal: 128224.69,
       cashBilledTotal: 69043.8,
-      nonCashBilledTotal: 59180.89,
-    },
+      nonCashBilledTotal: 59180.89
+    }
   });
 });
 
